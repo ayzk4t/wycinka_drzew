a83502f686df834da34b0ac2986d83863238b7aa
ayzk4t <75435115+ayzk4t@users.noreply.github.com>
2021-11-01 13:46:26 +0100

    Add files via upload

--- wycinka_drzew.py
+++ wycinka_drzew.py
@@ -1,3 +1,5 @@
+import itertools
+
 trees_numbers, min_space = input().split()
 trees_numbers = int(trees_numbers)
 min_space = int(min_space)
@@ -10,6 +12,8 @@ height = [int(h) for h in height]
 
 suspects = []
 cut_trees = []
+good = False
+min_height = max(height)
 
 for x in range(trees_numbers):
     if x == 0:
@@ -22,3 +26,10 @@ for x in range(trees_numbers):
         if ((position[x+1] - position[x]) < min_space) or \
                 ((position[x] - position[x-1]) < min_space):
             suspects.append(position[x])
+
+for c in range(1,len(suspects)):
+    combinations = list(itertools.combinations(suspects, c))
+    temp_list = suspects[:]
+    for comb in combinations:
+        for tree in comb:
+            temp_list.remove(tree)
